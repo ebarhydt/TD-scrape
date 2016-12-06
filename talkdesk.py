61e400aef4bf9ce7e18a15f17a888b6c49c0aff1
anlu <anlu@cloverintelligence.com>
2016-12-06 18:47:01 +0000

    Use logging instead of print

--- talkdesk.py
+++ talkdesk.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import datetime
+import logging
 import json
 import requests
 import time
+
+logging.basicConfig(format='[%(asctime)s] [%(filename)s:%(lineno)d] [%(levelname)s] %(message)s', level=logging.INFO)
 
 API_BASE_URL = 'https://api.talkdeskapp.com'
 LOGIN_URL = 'https://tawkify.mytalkdesk.com/users/sign_in'
@@ -22,8 +25,8 @@ DOWNLOAD_REP_LIST = (
     'Lindsey Perkins',
     'Kim Helmuth'
 )
-MIN_CALL_TIME = 400
-MAX_CALL_TIME = 3000
+MIN_CALL_TIME = 600
+MAX_CALL_TIME = 2400
 
 def authenticate():
     AUTH_REQUEST = {
@@ -37,7 +40,7 @@ def authenticate():
 
     r = requests.post(API_BASE_URL + '/auth/token', data=AUTH_REQUEST)
     if r.status_code != 200:
-        print 'Bad status code: %d' % r.status_code
+        logging.info('Bad status code on authenticate: %d' % r.status_code)
         exit()
 
     access_token = r.json()['access_token']
@@ -50,7 +53,7 @@ def create_report_job():
     from_time = '%s 00:00:00 UTC' % yesterday_str
     to_time = '%s 23:59:59 UTC' % yesterday_str
 
-    print 'Creating report for calls from %s to %s' % (from_time, to_time)
+    logging.info('Creating report for calls from %s to %s' % (from_time, to_time))
 
     r = requests.post(API_BASE_URL + '/reports/calls/jobs', headers=auth_header, data={
         'format': 'json',
@@ -58,23 +61,23 @@ def create_report_job():
         'timespan[to]': to_time
     })
     if r.status_code != 202:
-        print 'Bad status code: %d' % r.status_code
+        logging.info('Bad status code: %d' % r.status_code)
         exit()
     return r.json()
 
 if __name__ == '__main__':
     auth_header = authenticate()
-    print 'Successfully authenticated to API.'
+    logging.info('Successfully authenticated to API.')
     report_job = create_report_job()
-    print 'Created report with id ' + report_job['id']
+    logging.info('Created report with id ' + report_job['id'])
 
     while True:
         report = requests.get(API_BASE_URL + '/reports/calls/files/%s' % report_job['id'], headers=auth_header)
         if report.status_code == 200:
             report = report.json()
-            print 'Report fetched.'
+            logging.info('Report fetched.')
             break
-        print 'Report not ready yet, sleeping...'
+        logging.info('Report not ready yet, sleeping...')
         time.sleep(10)
 
     login_session = requests.Session()
@@ -89,17 +92,17 @@ if __name__ == '__main__':
         'commit': 'Login',
     })
     if login_response.status_code != 200:
-        print 'Error logging into Talkdesk to download recordings.'
+        logging.info('Error logging into Talkdesk to download recordings.')
         exit()
-    print 'Successfully logged in for downloading call mp3s.'
+    logging.info('Successfully logged in for downloading call mp3s.')
 
     for entry in report['entries']:
         if entry['agent_name'] in DOWNLOAD_REP_LIST and MIN_CALL_TIME <= entry['total_time'] <= MAX_CALL_TIME:
             if entry['recording_url'] is not None:
-                print 'Downloading call for %s from %s' % (entry['agent_name'], entry['recording_url'])
+                logging.info('Downloading call for %s from %s' % (entry['agent_name'], entry['recording_url']))
                 r = login_session.get(entry['recording_url'], stream=True)
                 with open('%s_%s.mp3' % (entry['agent_name'], entry['callsid']), 'wb') as fd:
                     for chunk in r.iter_content(chunk_size=1024):
                         fd.write(chunk)
             else:
-                print 'recording_url for %s was None' % entry['agent_name'],
+                logging.info('recording_url for %s was None' % entry['agent_name'])
